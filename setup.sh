e70b10c69c70eae0227accbd9b93cf1a51a5426e
SkyBlueEE <dahantianzi@gmail.com>
2019-07-19 18:03:02 +0800

    rsync link

--- setup.sh
+++ setup.sh
@@ -3,15 +3,15 @@ mkdir -p ~/.pip; cp pip.conf ~/.pip
 mkdir -p ~/.vim_undodir
 sudo mkdir -p ~root/.pip; sudo cp pip.conf ~root/.pip
 sudo apt install -y python-pip python3-pip
-sudo -H pip3 install cheat
-sudo -H pip3 install yapf
-sudo -H pip3 install pyftpdlib
-
 sudo apt install -y clang-format clang-tidy cppman
 sudo apt install -y autojump ranger
 sudo apt install -y vim sdcv lftp
 sudo apt install -y silversearcher-ag terminator
-ln terminator.config ~/.config/terminator/config
+mkdir -p ~/.config/terminator; ln terminator.config ~/.config/terminator/config
+
+sudo -H pip3 install cheat
+sudo -H pip3 install yapf
+sudo -H pip3 install pyftpdlib
 
 echo "source ~/rcfiles/_bashrc" >> ~/.bashrc
 echo "source ~/rcfiles/_vimrc" >> ~/.vimrc
