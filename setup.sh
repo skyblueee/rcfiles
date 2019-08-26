d2f0d2ec62dbdf2ca912a9e2f6f51a3dbbd82e70
SkyBlueEE <dahantianzi@gmail.com>
2019-08-26 22:51:08 +0800

    add qt qwt

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ mkdir -p ~/.pip; cp pip.conf ~/.pip
 mkdir -p ~/.vim_undodir
 sudo mkdir -p ~root/.pip; sudo cp pip.conf ~root/.pip
 sudo apt install -y python-pip python3-pip
-sudo apt install -y clang-format clang-tidy cppman clangd-8 libclang-dev
+sudo apt install -y clang-format clang-tidy cppman clangd-8 libclang-dev flake8
 sudo apt install -y autojump ranger ncdu
 sudo apt install -y vim sdcv lftp virtualbox virtualbox-guest-additions-iso
 sudo apt install -y silversearcher-ag terminator
